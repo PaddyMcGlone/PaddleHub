7c11dcc4014795a27c0c3e9653cca189575d2abb
paddymcglone <paddymcglone@gmail.com>
2018-01-31 18:07:32 +0000

    Creating PaddleFormViewModel

---
+++ PaddleHub/PaddleHub/ViewModels/PaddleFormViewModel.cs
@@ -0,0 +1,16 @@
+﻿
+namespace PaddleHub.ViewModels
+{
+    public class PaddleFormViewModel
+    {
+        #region Properties
+
+        public string Venue { get; set; }
+
+        public string Date { get; set; }
+
+        public string Time { get; set; }
+
+        #endregion
+    }
+}
