3719de9454ac8347b1cc20ccf540580d21cc8791
paddymcglone <paddymcglone@gmail.com>
2018-02-04 13:49:08 +0000

    Adding Post method to the create - This is to allow for the saving of posted date - Also adding helper method to controller for mapping - Adding datetime helper method for string parse

--- PaddleHub/PaddleHub/Controllers/PaddleController.cs
+++ PaddleHub/PaddleHub/Controllers/PaddleController.cs
@@ -1,4 +1,5 @@
-﻿using PaddleHub.Models;
+﻿using Microsoft.AspNet.Identity;
+using PaddleHub.Models;
 using PaddleHub.ViewModels;
 using System.Linq;
 using System.Web.Mvc;
@@ -24,5 +25,39 @@ namespace PaddleHub.Controllers
 
             return View(viewModel);
         }
+
+        [Authorize]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(PaddleFormViewModel viewModel)
+        {
+            if (!ModelState.IsValid) return View("Create", viewModel);
+            
+            var paddle = MapPaddle(viewModel);
+
+            context.Paddles.Add(paddle);
+            context.SaveChanges();
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        #region Helper methods
+
+        /// <summary>
+        /// Mapping helper method - will be moved out in future work.
+        /// </summary>
+        /// <param name="viewModel"></param>
+        /// <returns></returns>
+        private Paddle MapPaddle(PaddleFormViewModel viewModel)
+        {
+            return new Paddle
+            {
+                Paddler = new ApplicationUser { Id = User.Identity.GetUserId() },
+                DateTime = viewModel.PaddleDateTime(),
+                Location = viewModel.Location,
+                PaddleType = new PaddleType { Id = viewModel.PaddleType }
+            };
+        }
+
+        #endregion
     }
 }
--- PaddleHub/PaddleHub/ViewModels/PaddleFormViewModel.cs
+++ PaddleHub/PaddleHub/ViewModels/PaddleFormViewModel.cs
@@ -1,21 +1,58 @@
 ﻿
+using System;
 using System.Collections;
+using System.Globalization;
 
 namespace PaddleHub.ViewModels
 {
     public class PaddleFormViewModel
     {
         #region Properties
-
+        
+        /// <summary>
+        /// Gets or sets the location.
+        /// </summary>
         public string Location { get; set; }
 
+
+        /// <summary>
+        /// Gets or sets the date.
+        /// </summary>
         public string Date { get; set; }
 
+
+        /// <summary>
+        /// Gets or sets the time.
+        /// </summary>
         public string Time { get; set; }
         
-        public int PaddleType { get; set; }
 
+        /// <summary>
+        /// Gets or sets the paddletype.
+        /// </summary>
+        public byte PaddleType { get; set; }
+
+
+        /// <summary>
+        /// Gets or sets the paddle types list.
+        /// </summary>
         public IEnumerable PaddleTypes { get; set; }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Return formatted datetime
+        /// </summary>
+        /// <returns></returns>
+        public DateTime PaddleDateTime()
+        {
+            DateTime result;
+            DateTime.TryParseExact(Date + " " + Time, "g", CultureInfo.InvariantCulture, DateTimeStyles.None, out result);
+
+            return result;
+        }
 
         #endregion
     }
