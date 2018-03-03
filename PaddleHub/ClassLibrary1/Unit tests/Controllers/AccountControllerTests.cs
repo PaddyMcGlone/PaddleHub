f0f716234839d841f8a8bd461cbfde804c0cac26
paddymcglone <paddymcglone@gmail.com>
2018-03-03 14:50:48 +0000

    Adding the Account controller tests, these will have to be improved in the future

---
+++ PaddleHub/ClassLibrary1/Unit tests/Controllers/AccountControllerTests.cs
@@ -0,0 +1,35 @@
+﻿using PaddleHub.Controllers;
+
+namespace PaddleHubTests.Unit_tests.Controllers
+{
+    class AccountControllerTests
+    {
+        class Context
+        {            
+            public static AccountController controller;            
+
+            public Context()
+            {
+                controller = new AccountController();
+            }            
+        }
+
+        #region .Register(RegisterViewModel model) tests
+
+        class when_I_post_Register : Context
+        {
+            // Add NSubstitue to verify that UserManafer.CreateASync is called
+
+        }
+
+        class when_I_post_Register_with_model_state_errors : Context
+        {
+            // Model state is invald
+
+            // It returns the register view
+        }
+
+        #endregion
+
+    }
+}
