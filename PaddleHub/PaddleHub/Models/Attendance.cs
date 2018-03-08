d44046ceb68cf7a9a04f85ed216e30f0ad2404fc
paddymcglone <paddymcglone@gmail.com>
2018-03-08 21:01:10 +0000

    Adding the Attendance class to allow a user to attend a paddle

---
+++ PaddleHub/PaddleHub/Models/Attendance.cs
@@ -0,0 +1,35 @@
+﻿
+namespace PaddleHub.Models
+{
+    public class Attendance
+    {
+
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets Paddle Attendees
+        /// </summary>
+        public ApplicationUser Attendee { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Paddle event
+        /// </summary>
+        public Paddle Paddle { get; set; }
+
+        #endregion
+
+        #region Foreign Keys
+
+        /// <summary>
+        /// Gets or sets the Application user FK.
+        /// </summary>
+        public string AttendeeId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Paddle ID.
+        /// </summary>
+        public int PaddleID { get; set; }
+
+        #endregion
+    }
+}
