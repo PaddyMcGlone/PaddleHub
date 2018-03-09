047df6be0a779560c6df4f3af23fca35f8a1ffc2
paddymcglone <paddymcglone@gmail.com>
2018-03-09 20:27:12 +0000

    Adding initial webapi controller for managing attendances

---
+++ PaddleHub/PaddleHub/Controllers/AttendancesController.cs
@@ -0,0 +1,38 @@
+﻿using PaddleHub.Models;
+using System.Web.Http;
+
+namespace PaddleHub.Controllers
+{
+    public class AttendancesController : ApiController
+    {       
+        private ApplicationDbContext _context;
+
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public AttendancesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Attend method
+        /// </summary>
+        /// <param name="PaddleId">Paddle identifier</param>
+        /// <returns></returns>
+        public IHttpActionResult Attend(int paddleId)
+        {
+            var gig = _context.Paddles.Find(paddleId);
+
+            return Ok(200);
+        };
+
+        #endregion
+    }
+}
