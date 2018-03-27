eeedae85a7976bcd3242e656df9118197d197923
paddymcglone <paddymcglone@gmail.com>
2018-03-27 20:26:26 +0100

    Adding the new properties to the follow class

--- PaddleHub/PaddleHub/Models/Followers.cs
+++ PaddleHub/PaddleHub/Models/Followers.cs
@@ -1,8 +1,34 @@
 ﻿
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace PaddleHub.Models
 {
     public class Followers
     {
+        #region Navigational Properties
+        /// <summary>
+        /// Gets or sets the follower
+        /// </summary>
+        public ApplicationUser Follower { get; set; }
+
+        /// <summary>
+        /// Gets or sets the followee
+        /// </summary>
+        public ApplicationUser Followee { get; set; }
+        #endregion
+
+        #region Properties
+
+        [Key]
+        [Column(Order = 1)]
+        public string FollowerId { get; set; }
+
+        [Column(Order = 2)]
+        public string FolloweeId { get; set; }
+
+        #endregion
+
         
     }
 }
