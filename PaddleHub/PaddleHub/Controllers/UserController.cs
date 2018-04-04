15b8f06532fbd58a2844b69d5760df010315d045
paddymcglone <paddymcglone@gmail.com>
2018-04-04 20:21:58 +0100

    Implementing initial design of paddlers i am following

---
+++ PaddleHub/PaddleHub/Controllers/UserController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNet.Identity;
+using PaddleHub.Models;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace PaddleHub.Controllers
+{
+    public class UserController : Controller
+    {
+        #region Properties        
+        private ApplicationDbContext _context;
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// The default constructor
+        /// </summary>
+        public UserController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        #endregion
+
+        /// <summary>
+        /// The index action
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            return RedirectToAction("Following");
+        }
+
+        /// <summary>
+        /// The following or who am i following action
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Following()
+        {
+            var userId = User.Identity.GetUserId();
+
+            var following = _context.Followings
+                .Where(f => f.FollowerId == userId)
+                .Select(u => u.Followee)
+                .Include(u => u.UserDetails)
+                .ToList();
+
+            return View(following);
+
+        }
+    }
+}
