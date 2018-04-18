1c039e05f4f3144a1ca37138d1b654115d4602f8
paddymcglone <paddymcglone@gmail.com>
2018-04-18 20:25:52 +0100

    Adding the notification type enum

---
+++ PaddleHub/PaddleHub/Models/NotificationType.cs
@@ -0,0 +1,22 @@
+﻿
+namespace PaddleHub.Models
+{
+    public enum NotificationType
+    {
+        /// <summary>
+        /// Represents when an event is cancelled.
+        /// </summary>
+        Cancelled = 0,
+
+        /// <summary>
+        /// Represents an event being added.
+        /// </summary>
+        Added = 2,
+
+
+        /// <summary>
+        /// Represents an event being modified.
+        /// </summary>
+        Modified = 3
+    }
+}
