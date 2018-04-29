837bbd5db18910b37c0f0752cde178c864724a60
paddymcglone <paddymcglone@gmail.com>
2018-04-29 10:54:33 +0100

    Adding notification controller

---
+++ PaddleHub/PaddleHub/Controllers/API/NotificationController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Microsoft.AspNet.Identity;
+using PaddleHub.Models;
+
+namespace PaddleHub.Controllers.API
+{
+    [Authorize]
+    public class NotificationController : ApiController
+    {
+        private ApplicationDbContext context;
+
+        public NotificationController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public List<Notification> GetNewNotifications()
+        {
+            var userId = User.Identity.GetUserId();
+            var notifications = context.UserNotifcations
+                .Where(un => un.UserId == userId)
+                .Select(un => un.Notification)
+                .Include(n => n.Paddle.Paddler)
+                .ToList();
+
+            return notifications;
+        }
+    }
+}
