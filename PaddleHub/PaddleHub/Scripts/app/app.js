9a8544c7524a22843311437f679af725475f6df4
paddymcglone <paddymcglone@gmail.com>
2018-07-08 11:42:17 +0100

    Adding the module pattern to the javascript.

--- PaddleHub/PaddleHub/Scripts/app/app.js
+++ PaddleHub/PaddleHub/Scripts/app/app.js
@@ -1,31 +1,41 @@
-﻿function initPaddle() {
-    $(".js-toggle-attend").click(function(e) {
-        var button = $(e.target);
-        if (button.hasClass("btn-default")) {
-            $.post("/api/attendances", { "PaddleId": button.attr("data-id") })
-                .done(function() {
-                    button
-                        .removeClass("btn-default")
-                        .addClass("btn-info")
-                        .text("Attending");
-                })
-                .fail(function() {
-                    alert("Something went wrong");
-                });
-        } else {
-            $.ajax({
-                    url: "/api/attendances/" + button.attr("data-id"),
-                    method: "DELETE"
-                })
-                .done(function() {
-                    button
-                        .removeClass("btn-info")
-                        .addClass("btn-default")
-                        .text("Add to calender");
-                })
-                .fail(function() {
-                    alert("Something went wrong");
-                });
-        }
-    });
-}
+﻿var PaddleController = function() {
+
+    // The revealing module pattern
+    var init = function () {
+        // We have promoted the global function into a module.
+        $(".js-toggle-attend").click(function (e) {
+            var button = $(e.target);
+            if (button.hasClass("btn-default")) {
+                $.post("/api/attendances", { "PaddleId": button.attr("data-id") })
+                    .done(function () {
+                        button
+                            .removeClass("btn-default")
+                            .addClass("btn-info")
+                            .text("Attending");
+                    })
+                    .fail(function () {
+                        alert("Something went wrong");
+                    });
+            } else {
+                $.ajax({
+                        url: "/api/attendances/" + button.attr("data-id"),
+                        method: "DELETE"
+                    })
+                    .done(function () {
+                        button
+                            .removeClass("btn-info")
+                            .addClass("btn-default")
+                            .text("Add to calender");
+                    })
+                    .fail(function () {
+                        alert("Something went wrong");
+                    });
+            }
+        });
+    };
+
+    return{
+        init: init
+    }
+
+}(); //IIFE - Immediately invoked function expression
