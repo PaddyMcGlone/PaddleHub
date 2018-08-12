9d0fdade4c765b2ddef8a5efc5dd364aa4da1114
paddymcglone <paddymcglone@gmail.com>
2018-08-12 15:15:52 +0100

    Resolving copy paste error within the attendance service, declaration defined within incorrect function scope.

--- PaddleHub/PaddleHub/Scripts/app/services/AttendanceService.js
+++ PaddleHub/PaddleHub/Scripts/app/services/AttendanceService.js
@@ -1,12 +1,12 @@
-﻿var AttendanceService = function (paddleId, process, error) {
+﻿var AttendanceService = function () {
 
-    var createAttendance = function() {
+    var createAttendance = function (paddleId, process, error) {
         $.post("/api/attendances", { "PaddleId": paddleId })
             .done(process)
             .fail(error);
     };
 
-    var deleteAttendance = function() {
+    var deleteAttendance = function (paddleId, process, error) {
         $.ajax({
             url: "/api/attendances/" + paddleId,
                 method: "DELETE"
