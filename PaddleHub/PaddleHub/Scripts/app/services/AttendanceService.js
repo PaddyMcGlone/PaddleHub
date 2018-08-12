253622aad7382af8b489f877466aabb12f8ccfc5
paddymcglone <paddymcglone@gmail.com>
2018-08-12 12:05:55 +0100

    Correcting problem with the attendanceService.js module, incorrect IFFI setup.

--- PaddleHub/PaddleHub/Scripts/app/services/AttendanceService.js
+++ PaddleHub/PaddleHub/Scripts/app/services/AttendanceService.js
@@ -1,21 +1,21 @@
 ﻿var AttendanceService = function (paddleId, process, error) {
 
-    var createAttendance = function () {
+    var createAttendance = function() {
         $.post("/api/attendances", { "PaddleId": paddleId })
             .done(process)
             .fail(error);
-    }
+    };
 
-    var deleteAttendance = function () {
+    var deleteAttendance = function() {
         $.ajax({
                 url: "/api/attendances/" + paddleId,
                 method: "DELETE"
             })
             .done(process)
             .fail(error);
-    }
+    };
 
-    new {
+    return {
         CreateAttendance: createAttendance,
         DeleteAttendance: deleteAttendance
     };
