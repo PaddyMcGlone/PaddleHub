293f6fc6ec8632ecfa146297c722d52b37af8a21
paddymcglone <paddymcglone@gmail.com>
2018-09-04 18:44:52 +0100

    Create the repositories, adding the attendance repository method

---
+++ PaddleHub/PaddleHub/Repositories/AttendanceRepository.cs
@@ -0,0 +1,24 @@
+﻿using PaddleHub.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PaddleHub.Repositories
+{
+    public class AttendanceRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AttendanceRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        private IEnumerable<Attendance> GetFutureAttendances(string userId)
+        {
+            return _context.Attendances
+                .Where(a => a.AttendeeId == userId && a.Paddle.DateTime > DateTime.Now)
+                .ToList();
+        }
+    }
+}
---
+++ PaddleHub/PaddleHub/Repositories/PaddleRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PaddleHub.Repositories
+{
+    public class PaddleRepository
+    {
+    }
+}
